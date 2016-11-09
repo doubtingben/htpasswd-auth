241b5724f12031b88f222de1e2fca4d33e04e399
Ben Wilson <doubtingben@gmail.com>
2016-11-09 16:56:03 -0500

    add some requirements

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ if __name__ == "__main__":
     handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.DEBUG)
     app.logger.addHandler(handler)
-    app.run()
+    app.run(host='0.0.0.0')
